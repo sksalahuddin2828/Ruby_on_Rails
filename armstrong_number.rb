aef0954de4160e42134d2dfe85354de8b0b7d17c
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:34:13 +0600

    Create armstrong_number.rb

---
+++ armstrong_number.rb
@@ -0,0 +1,19 @@
+def is_armstrong_number(n)
+  num_digits = n.to_s.length
+  sum_of_powers = 0
+  temp = n
+  while temp > 0
+    digit = temp % 10
+    sum_of_powers += digit**num_digits
+    temp /= 10
+  end
+  n == sum_of_powers
+end
+
+puts "Enter a number:"
+user_input = gets.chomp.to_i
+if is_armstrong_number(user_input)
+  puts "#{user_input} is an Armstrong number."
+else
+  puts "#{user_input} is not an Armstrong number."
+end
