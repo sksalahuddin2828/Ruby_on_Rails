9c2b822080f56564f5770e4595de5869a32aa3c8
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:00:54 +0600

    Update armstrong_number.rb

--- armstrong_number.rb
+++ armstrong_number.rb
@@ -17,3 +17,21 @@ if is_armstrong_number(user_input)
 else
   puts "#{user_input} is not an Armstrong number."
 end
+
+#------------------------------------------------------------------
+
+def is_armstrong_number(n)
+  num_digits = n.to_s.length
+  sum_of_powers = 0
+  temp = n
+  while temp > 0
+    digit = temp % 10
+    sum_of_powers += digit**num_digits
+    temp /= 10
+  end
+  n == sum_of_powers
+end
+
+puts is_armstrong_number(153)  
+
+# Output: true
