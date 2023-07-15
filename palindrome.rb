6db691af1cad19b4f13f046c14a0f0e3925a87ae
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:41:16 +0600

    Update palindrome.rb

--- palindrome.rb
+++ palindrome.rb
@@ -10,3 +10,14 @@ if is_palindrome(user_input)
 else
   puts "'#{user_input}' is not a palindrome."
 end
+
+//-----------------------------------------------------------------------------
+
+function is_palindrome($s) {
+    $s = preg_replace("/[^0-9a-zA-Z]/", "", strtolower($s));
+    return $s === strrev($s);
+}
+
+echo is_palindrome("A man, a plan, a canal: Panama") ? 'true' : 'false';  
+
+// Output: true
