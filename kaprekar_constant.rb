afa47f67ce0ca625f328e3782d1504e44142f8a7
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:10:36 +0600

    Update kaprekar_constant.rb

--- kaprekar_constant.rb
+++ kaprekar_constant.rb
@@ -14,3 +14,21 @@ puts "Enter a number:"
 user_input = gets.chomp.to_i
 steps = kaprekar_constant(user_input)
 puts "Number of steps to reach Kaprekar constant: #{steps}"
+
+#-----------------------------------------------------------------------------
+
+def kaprekar_constant(n)
+  count = 0
+  while n != 6174
+    count += 1
+    digits = n.to_s.rjust(4, '0').chars
+    ascending = digits.sort.join.to_i
+    descending = digits.sort.reverse.join.to_i
+    n = descending - ascending
+  end
+  count
+end
+
+puts kaprekar_constant(1234)  
+
+# Output: 3
